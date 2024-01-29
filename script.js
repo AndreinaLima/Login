5b6d38279bc8b4d5b91d24653ef56dc9028360d5
Andreina Lima <153119703+AndreinaLima@users.noreply.github.com>
2024-01-29 15:30:57 -0300

    Update script.js
    comentarios

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
- 
+  // Função principal de validação do formulário
   function validateForm() {
     var email = document.getElementById("email").value
     var password = document.getElementById("password").value
     var confirmPassword = document.getElementById("confirm_password").value
 
-   
+    // Valida o email
     if (!validateEmail(email)) {
       alert("Por favor, insira um endereço de e-mail válido.")
       return false
     }
 
-    
+    // Verifica a correspondência das senhas
     if (password !== confirmPassword) {
       alert("As senhas não coincidem.")
       return false
     }
 
-    
+    // Se todas as validações passarem, o formulário pode ser enviado
     alert("Conta criada com sucesso!")
-   
+    // Aqui você pode redirecionar para a tela de login
     window.location.href = "index.html"
 
+    // Restante da sua lógica de envio do formulário
     return true
   }
 
- 
+  // Adiciona evento de clique ao botão de criar conta na tela de registro
   var createAccountButton = document.querySelector(
     ".register-card-form button[type='submit']"
   )
   if (createAccountButton) {
     createAccountButton.addEventListener("click", function (event) {
-      event.preventDefault() 
-      validateForm()
+      event.preventDefault() // Impede o envio do formulário por padrão
+      validateForm() // Chama a função de validação do formulário
     })
   }
 
- 
+  // Função auxiliar para alternar a visibilidade da senha
   function togglePasswordVisibility(inputField, showButton, hideButton) {
     if (inputField.type === "password") {
       inputField.type = "text"
@@ -49,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-
+  // Adiciona evento de clique aos botões de mostrar/esconder senha na tela de registro
   var showPasswordButton = document.getElementById("showPasswordButton")
   var hidePasswordButton = document.getElementById("hidePasswordButton")
   var passwordInput = document.getElementById("password")
@@ -102,7 +103,7 @@ document.addEventListener("DOMContentLoaded", function () {
     })
   }
 
-
+  // Função auxiliar para validar o formato do email
   function validateEmail(email) {
     var re = /\S+@\S+\.\S+/
     return re.test(email)
